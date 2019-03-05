8fa99a2c2aab7baf360649f80a28bdf0089eb39a
Jakub Kremláček <jkremlacek@email.cz>
2019-03-05 07:54:01 +0100

    Configured jar build and testing

---
+++ src/main/java/cz/mzk/archiveprocessor/App.java
@@ -0,0 +1,10 @@
+package cz.mzk.archiveprocessor;
+
+/**
+ * @author kremlacek
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/cz/mzk/archiveprocessor/AppTest.java
@@ -0,0 +1,16 @@
+package cz.mzk.archiveprocessor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author kremlacek
+ */
+public class AppTest {
+
+    //Placeholder test
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
