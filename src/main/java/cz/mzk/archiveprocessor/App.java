3be3d214280c542ef60f53b176c4b3587b0584d7
Jakub Kremláček <jkremlacek@email.cz>
2019-03-12 15:26:03 +0100

    App configuration implementation

--- src/main/java/cz/mzk/archiveprocessor/App.java
+++ src/main/java/cz/mzk/archiveprocessor/App.java
@@ -4,7 +4,16 @@ package cz.mzk.archiveprocessor;
  * @author kremlacek
  */
 public class App {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        run(args);
+    }
+
+    private static void run(String[] args) {
+        var cfg = AppConfiguration.getConfiguration(args);
+
+        if (cfg == null) {
+            System.exit(1);
+        }
     }
 }
---
+++ src/main/java/cz/mzk/archiveprocessor/AppConfiguration.java
@@ -0,0 +1,82 @@
+package cz.mzk.archiveprocessor;
+
+import java.io.File;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * @author kremlacek
+ */
+public class AppConfiguration {
+
+    private AppConfiguration() {}
+
+    private static final Options argumentOptions;
+    private static final CommandLineParser parser = new DefaultParser();
+    private static final HelpFormatter helpFormatter = new HelpFormatter();
+
+    private File inputDirectory;
+    private File outputDirectory;
+    private File errorDirectory;
+
+    static{
+        argumentOptions = new Options();
+
+        Option inputOpt = new Option("i", "input", true, "input directory path");
+        inputOpt.setRequired(true);
+
+        Option outputOpt = new Option("o", "output", true, "output directory path");
+        outputOpt.setRequired(true);
+
+        Option errorOpt = new Option("e", "error", true, "error directory path");
+        errorOpt.setRequired(true);
+
+        argumentOptions.addOption(inputOpt);
+        argumentOptions.addOption(outputOpt);
+        argumentOptions.addOption(errorOpt);
+    }
+
+    public static AppConfiguration getConfiguration(String[] args) {
+        var cfg = new AppConfiguration();
+
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(argumentOptions, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            helpFormatter.printHelp("utility-name", argumentOptions);
+            return null;
+        }
+
+        cfg.inputDirectory = new File(cmd.getOptionValue("input"));
+        cfg.outputDirectory = new File(cmd.getOptionValue("output"));
+        cfg.errorDirectory = new File(cmd.getOptionValue("error"));
+
+        checkDirectory(cfg.inputDirectory, false);
+        checkDirectory(cfg.outputDirectory, true);
+        checkDirectory(cfg.errorDirectory, true);
+
+        return cfg;
+    }
+
+    private static void checkDirectory(File directory, boolean writeRequired) {
+        if (!directory.exists()) {
+            throw new IllegalArgumentException("Directory does not exist. " + directory.getPath());
+        }
+
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException("Supplied path does not lead to directory" + directory.getPath());
+        }
+
+        if (writeRequired && !directory.canWrite()) {
+            throw new IllegalArgumentException("Cannot write into supplied directory" + directory.getPath());
+        }
+    }
+
+}
